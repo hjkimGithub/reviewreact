23609d2a3d6f76ac301e438f0d2241315c6b0214
김현재 <rlaguswo3408@gmail.com>
2022-12-13 10:55:52 +0900

    react-router-dom 5.3 -> 6

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/hello">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+      <Routes>
+        {/* <Route path="/hello" element={} /> */}
+        <Route path="/movie"  element={<Detail />}/>
+        <Route path="/" element={<Home />}  />
+      </Routes>
   );
 }
 
