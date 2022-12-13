c70ce0c4dda1a6bb1a98647a8ce4866114351c6c
김현재 <rlaguswo3408@gmail.com>
2022-12-13 20:09:09 +0900

    useCallback, 깃허브 페이지는 차후에 다시 해봐야 할듯,,, 폴더 별로 계정을 따로 써서 그런거같음

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
